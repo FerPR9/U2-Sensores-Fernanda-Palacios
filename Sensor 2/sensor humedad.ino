d97b8496117fc8fd8f7a6b0b6bcf5715345086c0
FerPR9 <126131443+FerPR9@users.noreply.github.com>
2023-03-09 23:42:04 -0600

    Update sensor humedad.ino

--- Sensor 2/sensor humedad.ino
+++ Sensor 2/sensor humedad.ino
@@ -1,1 +1,27 @@
+//Lectura de DHT22
+#include "DHT.h"
+#define pinDatos 4  //GPIO4 = D2 en D1 Mini, D4 en ESP32
+DHT sensorTH (pinDatos, DHT22);    //Crea objeto sensorTH
 
+void setup() {
+ Serial.begin (115200);
+ Serial.println ("***** Lectura DHT22 *****");
+ sensorTH.begin ();   //Inicializa pines del sensor
+ 
+}
+void loop() {
+  delay (2000);
+  
+  //Lee valores de temperatura y humedad
+  float humedad = sensorTH.readHumidity();
+  float temperatura = sensorTH.readTemperature();
+  
+  //Mostrar valores leídos
+  Serial.print ("Temperatura = ");
+  Serial.print (temperatura);
+  Serial.println (" ºC");
+  Serial.print ("Humedad = ");
+  Serial.print (humedad);
+  Serial.println (" %");
+  
+}
